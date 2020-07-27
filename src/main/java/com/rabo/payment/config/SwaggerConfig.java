089b6a3cc7d6d47308dbffe0e964a2ee98c9d493
Uma shankar <umashankar17693@gmail.com>
2020-07-27 19:48:49 +0530

    Swagger changes api restriction changes added

--- src/main/java/com/rabo/payment/config/SwaggerConfig.java
+++ src/main/java/com/rabo/payment/config/SwaggerConfig.java
@@ -18,8 +18,9 @@ public class SwaggerConfig {
 
 	@Bean
 	public Docket api() {
-		return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.any())
-				.paths(PathSelectors.any()).build();
+		return new Docket(DocumentationType.SWAGGER_2).select()
+				.apis(RequestHandlerSelectors.basePackage("com.rabo.payment.controller")).paths(PathSelectors.any())
+				.build();
 	}
 
 //	private ApiInfo apiEndPointsInfo() {
